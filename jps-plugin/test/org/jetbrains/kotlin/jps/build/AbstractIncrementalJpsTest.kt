4e8ccde6b28b544a656946792f28444a48853bf2
Ilya Ryzhenkov <orangy@jetbrains.com>
2015-01-13 00:38:28 +0300

    ?? delete file before copying to it, because new copyTo semantics defaults to non-overwrite mode.

--- jps-plugin/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalJpsTest.kt
+++ jps-plugin/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalJpsTest.kt
@@ -308,6 +308,7 @@ public abstract class AbstractIncrementalJpsTest : JpsBuildTestCase() {
             val file = File(workDir, path)
 
             val oldLastModified = file.lastModified()
+            file.delete()
             dataFile.copyTo(file)
 
             val newLastModified = file.lastModified()
