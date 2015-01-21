6f94a58be35f8c0129fe715cf2dd03b702e637f5
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-01-21 16:24:12 +0300

    No special logic in BuiltInsReferenceResolver

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/BuiltInsReferenceResolver.java
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/BuiltInsReferenceResolver.java
@@ -182,14 +182,6 @@ public class BuiltInsReferenceResolver extends AbstractProjectComponent {
 
     @Nullable
     private DeclarationDescriptor findCurrentDescriptorForClass(@NotNull ClassDescriptor originalDescriptor) {
-        //TODO:
-        // BindingContext doesn't contain an information about class descriptor of class object. For example see testEmptyRange.
-        if (DescriptorUtils.isClassObject(originalDescriptor)) {
-            DeclarationDescriptor currentParent = findCurrentDescriptor(originalDescriptor.getContainingDeclaration());
-            if (currentParent == null) return null;
-            return ((ClassDescriptor) currentParent).getClassObjectDescriptor();
-        }
-
         return bindingContext.get(BindingContext.FQNAME_TO_CLASS_DESCRIPTOR, DescriptorUtils.getFqName(originalDescriptor));
     }
 
