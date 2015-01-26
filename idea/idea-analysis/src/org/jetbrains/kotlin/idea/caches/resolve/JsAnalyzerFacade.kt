7a2e73cf1ac01713445ae5b7e0288678aed8f7b9
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-01-27 01:06:02 +0300

    ~ JsAnalyzerFacade

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/JsAnalyzerFacade.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/JsAnalyzerFacade.kt
@@ -33,6 +33,22 @@ import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.analyzer.ModuleContent
 import org.jetbrains.kotlin.js.resolve.KotlinJsCheckerProvider
 import org.jetbrains.kotlin.types.DynamicTypesAllowed
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.vfs.VirtualFile
+import java.io.IOException
+import org.jetbrains.kotlin.utils.LibraryUtils
+import java.io.InputStream
+import java.io.ByteArrayInputStream
+import java.util.ArrayList
+import org.jetbrains.kotlin.descriptors.PackageFragmentProvider
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.builtins.BuiltinsPackageFragment
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
+import org.jetbrains.kotlin.descriptors.impl.CompositePackageFragmentProvider
+import com.intellij.openapi.vfs.VirtualFileVisitor
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.util.PathUtil
 
 public class JsResolverForModule(
         override val lazyResolveSession: ResolveSession
@@ -58,8 +74,36 @@ public object JsAnalyzerFacade : AnalyzerFacade<JsResolverForModule, PlatformAna
         val injector = InjectorForLazyResolve(project, globalContext, moduleDescriptor, declarationProviderFactory, BindingTraceContext(),
                                               KotlinJsCheckerProvider, DynamicTypesAllowed())
         val resolveSession = injector.getResolveSession()!!
-        moduleDescriptor.initialize(resolveSession.getPackageFragmentProvider())
+        var packageFragmentProvider = resolveSession.getPackageFragmentProvider()
+
+        if (moduleInfo is LibraryInfo) {
+            val files = moduleInfo.library.getFiles(OrderRootType.CLASSES)
+            val metadataList: MutableList<LibraryUtils.Metadata> = arrayListOf()
+            for(file in files) {
+                metadataList.addAll(LibraryUtils.loadMetadataFromLibrary(PathUtil.getLocalPath(file)))
+            }
+            if (metadataList.size() > 0) {
+                val providers = ArrayList<PackageFragmentProvider>()
+                providers.add(packageFragmentProvider)
+                for (metadata in metadataList) {
+                    loadModuleDescriptors(moduleDescriptor, metadata, providers)
+                }
+                packageFragmentProvider = CompositePackageFragmentProvider(providers)
+            }
+        }
+        moduleDescriptor.initialize(packageFragmentProvider)
         return JsResolverForModule(resolveSession)
+    }
+
+    private fun loadModuleDescriptors(moduleDescriptor: ModuleDescriptorImpl, metadata: LibraryUtils.Metadata, providers: MutableList<PackageFragmentProvider>) {
+        val descriptors = metadata.files
+        val load = { (path: String) -> if (!descriptors.containsKey(path)) null else ByteArrayInputStream(descriptors.get(path)) }
+
+        for (sPackage in metadata.packages) {
+            val fqName = FqName(sPackage)
+            val builtinsPackageFragment = BuiltinsPackageFragment(fqName, LockBasedStorageManager(), moduleDescriptor, load)
+            providers.add(builtinsPackageFragment.provider)
+        }
     }
 
     override val defaultImports = TopDownAnalyzerFacadeForJS.DEFAULT_IMPORTS
